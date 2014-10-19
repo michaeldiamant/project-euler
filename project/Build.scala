a09209a63978ac76c520041b7b63d4716df4ecd7
michaeldiamant <michaeldiamant@users.noreply.github.com>
2014-10-18 22:01:25 -0400

    Add sbt build.

---
+++ project/Build.scala
@@ -0,0 +1,26 @@
+import sbt._
+import Keys._
+
+object ProjectEulerBuild extends Build {
+
+  val root =
+    Project(
+      id = "project-euler",
+      base = file("."),
+      settings =
+          Seq(
+            scalaVersion := "2.11.2",
+            scalacOptions ++=
+              Seq(
+                "-unchecked",
+                "-deprecation",
+                "-feature",
+                "-Xlint",
+                "-language:reflectiveCalls"),
+            testOptions in Test := Seq(Tests.Filter(s => s.endsWith("Spec"))),
+            resolvers += "Scalaz Bintray Repo" at "http://dl.bintray.com/scalaz/releases",
+            libraryDependencies ++=
+              Seq(
+                "org.specs2" %% "specs2" % "2.4.6" % "test",
+                "org.scalacheck" %% "scalacheck" % "1.11.6" % "test")))
+}
