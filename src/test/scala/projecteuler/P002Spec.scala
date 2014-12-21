342c6c71b636fdefd5a2795b150e9a36d1424a4d
mdiamant <diamant.michael@gmail.com>
2014-12-20 21:30:50 -0500

    Add test to ensure eager and lazy solutions to problem 2 produce same answer.

--- src/test/scala/projecteuler/P002Spec.scala
+++ src/test/scala/projecteuler/P002Spec.scala
@@ -1,15 +1,27 @@
 package projecteuler
 
+import org.scalacheck.{Prop, Gen}
+import org.specs2.ScalaCheck
 import org.specs2.mutable.Specification
 
-class P002Spec extends Specification {
+class P002Spec extends Specification with ScalaCheck {
 
   "Fibonacci sequence with numbers < 90" should {
-
     "be 1, 2, 3, 5, 8, 13, 21, 34, 55, 89" >> {
       P002.fibonacciUntil(max = 90) ====
         1 :: 2 :: 3 :: 5 :: 8 :: 13 :: 21 :: 34 :: 55 :: 89 :: Nil
     }
 
+  }
+
+  "Eager and lazy implementations" should {
+    "yield the same result" >> {
+      def max = Gen.choose(1, 10000)
+      Prop.forAllNoShrink(max) {
+        max =>
+          P002.sumEvenValuedFibonacciUntil(max) ====
+            P002.lazilySumEvenValuedFibonacciUntil(max)
+      }
+    }
   }
 }
